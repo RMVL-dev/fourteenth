d9af7ad9c0429446078aea2b61a5880f2659d150
ramilmavlihanov <Ramil040201@gmail.com>
2024-12-31 13:35:33 +0300

    module_14_2 done

---
+++ module_14_2.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
+
+cursor.execute("Select COUNT(*) From Users")
+count = cursor.fetchone()[0]
+cursor.execute("Select SUM(balance) From Users")
+balance_sum = cursor.fetchone()[0]
+cursor.execute("Select AVG(balance) From Users")
+avg_balance_auto = cursor.fetchone()[0]
+
+print(f"auto average balance = {avg_balance_auto}\nmanual average balance = {balance_sum/count}")
+
+
+connection.close()
